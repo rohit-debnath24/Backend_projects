5a7db56503f7e6bcf3135d519c0242d5dafb7452
Rohit Debnath <rohit.kumardebnath.cse28@heritageit.edu.in>
2025-06-20 03:01:17 +0530

    Project-ready except-Edit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const app =express();
 const path= require('path');
+const userModel=require('./models/user');
 
 app.set("view engine","ejs");
 app.use(express.json());
@@ -14,12 +15,30 @@ app.get('/',(req,res)=>{
     res.render("index");
     
 })
-app.get('/read',(req,res)=>{
-    res.render("read");
+app.get('/read',async (req,res)=>{
+
+   let users =  await userModel.find();
+    res.render("read",{users});
     
 })
-app.post('/create',(req,res)=>{
-    res.render("read");
+app.get('/delete/:id',async (req,res)=>{
+
+   let userDeleted =  await userModel.findOneAndDelete({_id:req.params.id});
+    res.redirect("/read");
+    
+})
+app.post('/create',async (req,res)=>{
+    let{name,email,image}=req.body;
+
+    let createdUser= await userModel.create({
+        name,
+        email,
+        image
+
+
+    })
+
+    res.redirect("/read");
     
 })
 app.listen(3000);
